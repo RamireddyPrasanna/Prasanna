9877a2126547737d3da61044ab69d8330b7cc3ca
RamireddyPrasanna <ramireddyprasanna45@gmail.com>
2024-07-06 16:27:29 +0530

    Add files via upload

---
+++ forms.js
@@ -0,0 +1,20 @@
+const express=require('express');
+var app=express();
+app.use(express.static('public'));
+app.get('/form.html',function(req,res){
+    res.sendFile(__dirname+"/"+"form.html");
+
+})
+app.get('/process_get',function(req,res){
+    var response={
+        first_name:req.query.first_name,
+        last_name:req.query.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+var server=app.listen(8000,function(){
+    var host=server.address().address
+    var port=server.address().port
+    console.log("Example app listening at http://%s:%s",host,port)
+})
